fb8e6535fc0ea03322b5762c8a0e75eca5f7ac6e
Beyonder <alidelhiindia@gmail.com>
2024-02-19 20:49:55 +0530

    Organising files

--- hw2/target/debug/hw2.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/alide/Desktop/ICP/ICP_Bootcamp_2/hw2/target/debug/hw2: /mnt/c/Users/alide/Desktop/ICP/ICP_Bootcamp_2/hw2/src/main.rs
---
+++ target/debug/hw2.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/alide/Desktop/ICP/ICP_Bootcamp_2/target/debug/hw2: /mnt/c/Users/alide/Desktop/ICP/ICP_Bootcamp_2/src/main.rs
